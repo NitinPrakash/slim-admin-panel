bc4fba7b651a90df854b0566fcdbfd13b676698c
Nitin Prakash <nitin247@outlook.com>
2021-04-16 23:59:11 +0530

    Updated config file

--- config.php
+++ config.php
@@ -1,14 +1,15 @@
 <?php 
-    session_start();
+    //session_start();
     $app = new \Slim\Slim();
-    $app->add(new \Slim\Middleware\SessionCookie(array('secret' => 'sewasankalp')));
+    $app->add(new \Slim\Middleware\SessionCookie(array('secret' => 'slimadmin')));
 	 
     $REQUEST = $app->request();
-    $pdo = new PDO("mysql:host=localhost;dbname=sewa_admin", 'root', '');
+    $pdo = new PDO("mysql:host=localhost;dbname=slim_admin", 'root', '123456');
     
-    define( 'SITENAME','Sewa Sankalp' );
+    define( 'SITENAME','Slim Admin' );
     define( 'SITEROOT',$REQUEST->getRootUri() );
     define( 'RESOURCEURI',$REQUEST->getResourceUri() );
+    define( 'BASE_URL','http://loaclhost/slim-admin-panel' );
         
     $array_resource_uri = explode( '/',RESOURCEURI );
     $base_file = empty($array_resource_uri[1])?'home':$array_resource_uri[1];
